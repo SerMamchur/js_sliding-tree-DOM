f7b3c68724b940cd9f568df91983879dab1fc14b
Serhii Mamchur <kovaniysvit@gmail.com>
2025-03-26 13:04:27 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const li = document.querySelectorAll('.tree > li > ul > li');
+
+li.forEach((el) => {
+  if (el.textContent) {
+    const spanEl = document.createElement('span');
+    const text = el.firstChild;
+
+    spanEl.textContent = el.firstChild.textContent;
+    el.firstChild.textContent = '';
+
+    el.firstChild.parentNode.insertBefore(spanEl, text);
+    el.style.display = 'block';
+  }
+});
+
+addEventListener('click', (e) => {
+  const clickOn = e.target;
+
+  if (clickOn.tagName !== 'SPAN') {
+    return;
+  }
+
+  const ul = clickOn.parentNode.querySelector('ul');
+
+  if (ul) {
+    if (ul.style.display === 'block') {
+      ul.style.display = 'none';
+    } else {
+      ul.style.display = 'block';
+    }
+  }
+});
